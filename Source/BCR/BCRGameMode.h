7cc504c83add683cf865c55b7dbb23749a7a2143
Thibaut LE PERF <thibaut.leperf@ynov.com>
2024-10-01 16:13:07 +0200

    [ADD] Initial Commit
    Adding UE5 third personn template

---
+++ Source/BCR/BCRGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BCRGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABCRGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABCRGameMode();
+};
+
+
+
