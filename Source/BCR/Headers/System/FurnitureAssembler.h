dd8912169b84933eda6bd44fa165be0bde0c6540
cajou42 <80094480+cajou42@users.noreply.github.com>
2025-01-30 09:30:57 +0100

    ADD : V0.5 of the furniture Assembleur + begining Ui system

---
+++ Source/BCR/Headers/System/FurnitureAssembler.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "DynamicMesh/ColliderMesh.h"
+#include "GameFramework/Actor.h"
+#include "BCR/Headers/System/Pickable/PickableItem.h"
+#include "BCR/Headers/Interfaces/Interactable.h"
+#include "FurnitureAssembler.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRecipiesInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<TSubclassOf<APickableItem>, int> Material;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> Out;
+};
+
+UCLASS()
+class BCR_API AFurnitureAssembler : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFurnitureAssembler();
+	UFUNCTION(BlueprintCallable)
+	void CraftFurniture();
+protected:
+
+	UBoxComponent* AssemblerZone;
+	UDataTable* AllRecipies;
+	TArray<FName> RowsNames;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// Interface Methods
+	void Interact_Implementation(AMainPlayer* Player);
+	void InteractWithObject_Implementation(AMainPlayer* Player, AActor* Object);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRecipiesInfo ActualRecipies;
+};
+
+
---
+++ Source/BCR/Sources/System/FurnitureAssembler.cpp
@@ -0,0 +1,92 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BCR/Headers/System/FurnitureAssembler.h"
+
+
+// Sets default values
+AFurnitureAssembler::AFurnitureAssembler()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// Create Collision Box
+	AssemblerZone = CreateDefaultSubobject<UBoxComponent>(FName("Collision Zone"));
+	AssemblerZone->SetRelativeScale3D(FVector(10, 10, 2));
+	AssemblerZone->SetVisibility(true);
+	AssemblerZone->SetHiddenInGame(false);
+	AssemblerZone->SetVisibleInSceneCaptureOnly(false);
+
+	// Add event to the OnComponentBeginOverlap delegate
+	RootComponent = AssemblerZone;
+	AssemblerZone->OnComponentBeginOverlap.AddDynamic(this, &AFurnitureAssembler::OnOverlapBegin);
+
+	// Keep in memory the first recipie
+	AllRecipies = LoadObject<UDataTable>(nullptr, TEXT("/Game/Becorn/Data/DT_RecipiesList.DT_RecipiesList"));
+	if (AllRecipies)
+	{
+		RowsNames = AllRecipies->GetRowNames();
+		ActualRecipies = *AllRecipies->FindRow<FRecipiesInfo>(RowsNames[0], TEXT(""));
+		RowsNames.RemoveAt(0,1);
+	}
+}
+
+// Called when the game starts or when spawned
+void AFurnitureAssembler::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFurnitureAssembler::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Poup"));
+}
+
+void AFurnitureAssembler::CraftFurniture()
+{
+	int valideIngredients = 0;
+	for (auto Ingredient : ActualRecipies.Material)
+	{
+		if (ActualRecipies.Material[Ingredient.Key] != 0)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::Printf(TEXT("POIREAUX")));
+			return;
+		}
+	}
+
+	auto temp  = GetWorld()->SpawnActor<AActor>(ActualRecipies.Out);
+	temp->SetActorLocation(GetActorLocation());
+}
+
+void AFurnitureAssembler::Interact_Implementation(AMainPlayer* Player)
+{
+}
+
+void AFurnitureAssembler::InteractWithObject_Implementation(AMainPlayer* Player, AActor* Object)
+{
+	if (Cast<IIPickable>(Object))
+	{
+		IIPickable::Execute_Drop(Object, this, Object);
+		auto requieredMaterials = ActualRecipies.Material.Find(Object->GetClass());
+		if (requieredMaterials && requieredMaterials[0] != 0)
+		{
+			requieredMaterials[0]--;
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::Printf(TEXT("requiered = %d"), requieredMaterials[0]));
+			Object->Destroy();
+		}
+		else
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::Printf(TEXT("ouille")));
+			Object->Destroy();
+		}
+
+	}
+}
+
+// Called every frame
+void AFurnitureAssembler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
