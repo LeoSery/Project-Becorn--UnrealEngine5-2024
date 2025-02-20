5f34eef703162b97f26a349f949f01a0b3fd337a
Thibaut LE PERF <thibaut.leperf@ynov.com>
2025-02-20 11:29:02 +0100

    Binaires

--- Intermediate/Build/Win64/UnrealEditor/Inc/BCR/UHT/BCR.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/BCR/UHT/BCR.init.gen.cpp
@@ -27,7 +27,7 @@ void EmptyLinkFunctionForGeneratedCodeBCR_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x2D70ACA0,
+				0x8B51C49A,
 				0x41B7CE35,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -35,5 +35,5 @@ void EmptyLinkFunctionForGeneratedCodeBCR_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_BCR.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BCR(Z_Construct_UPackage__Script_BCR, TEXT("/Script/BCR"), Z_Registration_Info_UPackage__Script_BCR, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x2D70ACA0, 0x41B7CE35));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BCR(Z_Construct_UPackage__Script_BCR, TEXT("/Script/BCR"), Z_Registration_Info_UPackage__Script_BCR, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8B51C49A, 0x41B7CE35));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
