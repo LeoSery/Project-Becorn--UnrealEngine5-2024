fc2d36f0be7533571f388e446b032e78520e4a91
cajou42 <80094480+cajou42@users.noreply.github.com>
2025-03-04 19:25:52 +0100

    ADD : Assembler UI + begin rework ropes

---
+++ Source/BCR/Headers/System/UniqueRope.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BCR/Headers/Interfaces/Interactable.h"
+#include "UniqueRope.generated.h"
+
+UCLASS()
+class BCR_API AUniqueRope : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AUniqueRope();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Interface Methods
+	void Interact_Implementation(AMainPlayer* Player);
+	void InteractWithObject_Implementation(AMainPlayer* Player, AActor* Object);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
