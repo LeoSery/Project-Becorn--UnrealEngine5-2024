734b3db78ad7100a519921638939d20b4e8af950
LeoSery <74504632+LeoSery@users.noreply.github.com>
2025-06-26 14:36:48 +0200

    Update: version for build tracking and Add: Project and Game custom becorn splash screen.

--- Intermediate/Build/Win64/UnrealEditor/Inc/BCR/UHT/BCR.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/BCR/UHT/BCR.init.gen.cpp
@@ -31,7 +31,7 @@ void EmptyLinkFunctionForGeneratedCodeBCR_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x3017CA50,
+				0x5F71465B,
 				0x599162D1,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -39,5 +39,5 @@ void EmptyLinkFunctionForGeneratedCodeBCR_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_BCR.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BCR(Z_Construct_UPackage__Script_BCR, TEXT("/Script/BCR"), Z_Registration_Info_UPackage__Script_BCR, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3017CA50, 0x599162D1));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BCR(Z_Construct_UPackage__Script_BCR, TEXT("/Script/BCR"), Z_Registration_Info_UPackage__Script_BCR, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5F71465B, 0x599162D1));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
