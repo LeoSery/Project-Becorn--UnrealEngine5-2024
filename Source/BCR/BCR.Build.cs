fb104172a4de52dead78892218bdcfadd96c4a15
LeoSery <74504632+LeoSery@users.noreply.github.com>
2025-08-15 11:19:09 +0200

    Add: New Unit tests with new method.

--- Source/BCR/BCR.Build.cs
+++ Source/BCR/BCR.Build.cs
@@ -7,5 +7,7 @@ public class BCR : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		
+		PublicDependencyModuleNames.AddRange(new string[] { "UnrealEd" });
 	}
 }
---
+++ Source/BCR/Sources/Test/PlayerTests.cpp
@@ -0,0 +1,21 @@
+﻿#include "Misc/AutomationTest.h"
+#include "Tests/AutomationEditorCommon.h"
+#include "BCR/Headers/Player/MainPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(PlayerTests, "BCR.BCR.Sources.Test.PlayerTests",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool PlayerTests::RunTest(const FString& Parameters)
+{
+	UWorld* world = FAutomationEditorCommonUtils::CreateNewMap();
+	
+	AMainPlayer* player = world->SpawnActor<AMainPlayer>();
+
+	if (player->GetCharacterMovement()->JumpZVelocity != 700.f)
+	{
+		AddError(TEXT("player velocity does not match the correct value."));
+	}
+	
+	return true;
+}
